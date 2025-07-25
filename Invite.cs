c5424766d89fa97a32418da06e71c063f3dcaa77
Enzo Caruso <caruso.enzo42000@gmail.com>
2025-07-25 16:32:49 +0200

    Affichage invite dans detail formulaire

---
+++ Invite.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arkone_Logiciel_Evenementiel.Models;
+
+public partial class Invite
+{
+    public override string ToString()
+    {
+        return (this.Nom + " " + this.Prenom);
+    }
+}
