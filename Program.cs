0905d4b35c2c9a28833c9fa8e88f8bb918ea2595
Enzo Caruso <caruso.enzo42000@gmail.com>
2025-07-25 09:37:20 +0200

    test push

--- Program.cs
+++ Program.cs
@@ -12,6 +12,8 @@ namespace Arkone_Logiciel_Evenementiel
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
             Application.Run(new Form1());
+
+
         }
     }
 }
